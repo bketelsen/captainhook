1e51ce80e98e4b4a54f60a37782080d85b52fd18
Kel Cecil <kelcecil@praisechaos.com>
2015-01-10 17:26:43 -0500

    Expose POST data to scripts

--- hook.go
+++ hook.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -18,6 +20,7 @@ func hookHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 		return
 	}
+	interoplatePOSTData(rb, r)
 	response, err := rb.execute()
 	if err != nil {
 		log.Println(err.Error())
@@ -30,5 +33,24 @@ func hookHandler(w http.ResponseWriter, r *http.Request) {
 			log.Println(err.Error())
 		}
 		w.Write(data)
+	}
+}
+
+func interoplatePOSTData(rb *runBook, r *http.Request) {
+	if r.ContentLength == 0 {
+		return
+	}
+	data := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(data)
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+		return
+	}
+	defer r.Body.Close()
+	stringData := string(data[:r.ContentLength])
+	for i := range rb.Scripts {
+		for j := range rb.Scripts[i].Args {
+			rb.Scripts[i].Args[j] = strings.Replace(rb.Scripts[i].Args[j], "{{POST}}", stringData, -1)
+		}
 	}
 }
