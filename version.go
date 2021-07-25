073a26a793b9b20eb3f701131037989cddf6e737
Brian Ketelsen <bketelsen@gmail.com>
2021-07-25 05:42:33 -0400

    version bump: v0.1.22

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 // Version is the tagged version or "dev"
-var Version = "v0.1.21"
+var Version = "v0.1.22"
 
 // BuildDate is the date of the release build or ""
 var BuildDate = ""
