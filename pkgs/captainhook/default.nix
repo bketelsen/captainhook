f2c5a781246cf5a8e1cf273ddc95a51eccea8104
brian ketelsen <bketelsen@gmail.com>
2022-10-14 04:59:03 -0400

    floxify project

---
+++ pkgs/captainhook/default.nix
@@ -0,0 +1,18 @@
+{
+  self,
+  buildGoModule,
+  lib,
+  pkgs
+}:
+buildGoModule {
+  pname = "captainhook";
+  version = "0.0.0";
+  src = self; # + "/src";
+  # vendorSha256 should be set to null if dependencies are vendored. If the dependencies aren't
+  # vendored, vendorSha256 must be set to a hash of the content of all dependencies. This hash can
+  # be found by setting
+  # vendorSha256 = lib.fakeSha256;
+  # and then running flox build. The build will fail but output the expected sha, which can then be
+  # added here.
+  vendorSha256 = "sha256-Ry+sbQj6eMccu4S0rHeBAZlsHTGkjBa0ZVqebIEJh9g=";
+}
